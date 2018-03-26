b80df22e8306b74e1c75f4a4c7f10d317bc67d40
Manfredi_Napolitano <manfnapolitano@gmail.com>
2018-03-26 17:13:53 +0200

    ho modificato il progetto di peppe con un commento

--- app/src/main/java/com/example/giuseppe/contatore/MainActivity.java
+++ app/src/main/java/com/example/giuseppe/contatore/MainActivity.java
@@ -52,6 +52,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
         //ciao
+        //ciao peppe
         vAzzera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
