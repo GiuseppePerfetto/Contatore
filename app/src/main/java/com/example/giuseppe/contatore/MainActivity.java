be549f669b907d6f36ab63ce0592b29c905a1b74
Giuseppe Perfetto <giuseppe.perfetto26@gmail.com>
2018-03-26 16:59:38 +0200

    Ultimo aggiornamento

--- app/src/main/java/com/example/giuseppe/contatore/MainActivity.java
+++ app/src/main/java/com/example/giuseppe/contatore/MainActivity.java
@@ -51,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
+        //ciao
         vAzzera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
