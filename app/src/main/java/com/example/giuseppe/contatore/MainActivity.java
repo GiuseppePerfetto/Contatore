bb10c28adc64107852bf38dcff828dda022f040a
Giuseppe Perfetto <giuseppe.perfetto26@gmail.com>
2018-03-26 15:30:32 +0200

    Ho fatto i riferimenti agli oggetti

--- app/src/main/java/com/example/giuseppe/contatore/MainActivity.java
+++ app/src/main/java/com/example/giuseppe/contatore/MainActivity.java
@@ -4,10 +4,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView vMessaggio;
+    private Button vAumenta;
+    private Button vDiminuisci;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        vMessaggio = findViewById(R.id.Textmessaggio);
+        vAumenta= findViewById(R.id.button);
+        vDiminuisci= findViewById (R.id.button2);
     }
 }
