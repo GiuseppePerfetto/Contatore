c388c186ddbede33cfa6f62094eebefdd026649c
Giuseppe Perfetto <giuseppe.perfetto26@gmail.com>
2018-03-26 15:50:46 +0200

    Prima versione funzionante

--- app/src/main/java/com/example/giuseppe/contatore/MainActivity.java
+++ app/src/main/java/com/example/giuseppe/contatore/MainActivity.java
@@ -2,11 +2,16 @@ package com.example.giuseppe.contatore;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private TextView vMessaggio;
     private Button vAumenta;
     private Button vDiminuisci;
+
+    private int Contatore=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +21,32 @@ public class MainActivity extends AppCompatActivity {
         vMessaggio = findViewById(R.id.Textmessaggio);
         vAumenta= findViewById(R.id.button);
         vDiminuisci= findViewById (R.id.button2);
+
+        vAumenta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Contatore++;
+                VisualizzaMessaggio();
+
+            }
+        });
+
+        vDiminuisci.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Contatore--;
+                VisualizzaMessaggio();
+
+            }
+        });
+    }
+
+    /**
+     * funzione che mostra il conteggio.
+     */
+    private void VisualizzaMessaggio(){
+        vMessaggio.setText(getString(R.string.valore) + "  " + Contatore );
+
+
     }
 }
